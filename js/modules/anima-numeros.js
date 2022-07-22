6eca8b40c856d979a8fcceb77a3ea3bc1d88ea87
edineipinho <edneyrock@gmail.com>
2022-07-22 16:56:56 -0300

    Adicionado Webpack e Babel

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -1,30 +1,31 @@
 export default function initAnimaNumeros() {
   function animaNumeros() {
-    const numeros = document.querySelectorAll("[data-numero");
+    const numeros = document.querySelectorAll('[data-numero]');
 
     numeros.forEach((numero) => {
       const total = +numero.innerText;
-      const incremento = Math.floor(total / 100);
+      const inscremento = Math.floor(total / 100);
       let start = 0;
       const timer = setInterval(() => {
-        start += incremento;
+        start += inscremento;
         numero.innerText = start;
         if (start > total) {
-          clearInterval(timer);
           numero.innerText = total;
+          clearInterval(timer);
         }
       }, 25 * Math.random());
     });
   }
 
+  let observer;
   function handleMutation(mutation) {
-    if (mutation[0].target.classList.contains("ativo")) {
+    if (mutation[0].target.classList.contains('ativo')) {
       observer.disconnect();
       animaNumeros();
     }
   }
+  observer = new MutationObserver(handleMutation);
 
-  const observeTarget = document.querySelector(".numeros");
-  const observer = new MutationObserver(handleMutation);
-  observer.observe(observeTarget, { attributes: true });
+  const observerTarget = document.querySelector('.numeros');
+  observer.observe(observerTarget, { attributes: true });
 }
--- js/modules/tooltip.js
+++ js/modules/tooltip.js
@@ -1,40 +1,42 @@
 export default function initTooltip() {
-  const tooltips = document.querySelectorAll("[data-tooltip]");
+  const tooltips = document.querySelectorAll('[data-tooltip]');
 
-  tooltips.forEach((item) => {
-    item.addEventListener("mouseover", onMouseOver);
-  });
-
-  function onMouseOver(event) {
-    const tooltipBox = criarTooltipBox(this);
-    onMouseMove.tooltipBox = tooltipBox;
-    this.addEventListener("mousemove", onMouseMove);
-    onMouseLeave.tooltipBox = tooltipBox;
-    onMouseLeave.element = this;
-    this.addEventListener("mouseleave", onMouseLeave);
-  }
+  const onMouseMove = {
+    handleEvent(event) {
+      this.tooltipBox.style.top = `${event.pageY + 20}px`;
+      this.tooltipBox.style.left = `${event.pageX + 20}px`;
+    },
+  };
 
   const onMouseLeave = {
     handleEvent() {
       this.tooltipBox.remove();
-      this.element.removeEventListener("mouseleave", onMouseLeave);
-      this.element.removeEventListener("mousemove", onMouseMove);
-    },
-  };
-
-  const onMouseMove = {
-    handleEvent(event) {
-      this.tooltipBox.style.top = event.pageY + 20 + "px";
-      this.tooltipBox.style.left = event.pageX + 20 + "px";
+      this.element.removeEventListener('mouseleave', onMouseLeave);
+      this.element.removeEventListener('mousemove', onMouseMove);
     },
   };
 
   function criarTooltipBox(element) {
-    const tooltipBox = document.createElement("div");
-    const texto = element.getAttribute("aria-label");
-    tooltipBox.classList.add("tooltip");
+    const tooltipBox = document.createElement('div');
+    const text = element.getAttribute('aria-label');
+    tooltipBox.classList.add('tooltip');
+    tooltipBox.innerText = text;
     document.body.appendChild(tooltipBox);
-    tooltipBox.innerText = texto;
     return tooltipBox;
   }
+
+  function onMouseOver() {
+    const tooltipBox = criarTooltipBox(this);
+
+    onMouseMove.tooltipBox = tooltipBox;
+    this.addEventListener('mousemove', onMouseMove);
+
+    onMouseLeave.tooltipBox = tooltipBox;
+    onMouseLeave.element = this;
+    this.addEventListener('mouseleave', onMouseLeave);
+  }
+
+  tooltips.forEach((item) => {
+    item.addEventListener('mouseover', onMouseOver);
+  });
 }
