069eefc4b68c47eb8edf73e73044a3ae6022be39
edineipinho <edneyrock@gmail.com>
2022-07-24 08:14:33 -0300

    refactor(scroll-anima.js): refatoração com class

--- js/script.js
+++ js/script.js
@@ -8,27 +8,33 @@ import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
-import initAnimacaoScroll from './modules/scroll-animacao.js';
+import ScrollAnima from './modules/scroll-anima.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
+
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
+
 const tabNav = new TabNav(
   '[data-tab="menu"] li',
   '[data-tab="content"] section',
 );
 tabNav.init();
+
 const modal = new Modal(
   '[data-modal="abrir"]',
   '[data-modal="fechar"]',
   '[data-modal="container"]',
 );
 modal.init();
+
 const tooltip = new Tooltip('[data-tooltip]');
 tooltip.init();
 
-initAnimacaoScroll();
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
+scrollAnima.init();
+
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
